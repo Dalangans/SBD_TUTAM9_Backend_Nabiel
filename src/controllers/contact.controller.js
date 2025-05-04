88f18f595cad787a4bf23d3e3eae35321ea58d7a
Nabiel Utomo <nabielutomo@gmail.com>
2025-05-04 21:52:13 +0700

    First Commit

---
+++ src/controllers/contact.controller.js
@@ -0,0 +1,52 @@
+const contactRepository = require("../repositories/contact.repository");
+
+exports.createContact = async (req, res) => {
+    const { name, phone } = req.body;
+    try {
+        const newContact = await contactRepository.createContact({ name, phone });
+        return baseResponse(res, true, 201, "Contact created", {
+            ...newContact,
+            created_at: newContact.created_at,
+            updated_at: newContact.updated_at,
+        });
+    } catch (error) {
+        return baseResponse(res, false, 500, "Error creating contact", error);
+    }
+};
+
+exports.getContacts = async (req, res) => {
+    try {
+        const contacts = await contactRepository.getContacts();
+        return baseResponse(res, true, 200, "Contacts found", contacts);
+    } catch (error) {
+        return baseResponse(res, false, 500, "Error retrieving contacts", error);
+    }
+};
+
+exports.updateContact = async (req, res) => {
+    const { id, name, phone } = req.body;
+    try {
+        const contact = await contactRepository.updateContact(id, { name, phone });
+        if (!contact) {
+            return baseResponse(res, false, 404, "Contact not found", null);
+        }
+        return baseResponse(res, true, 200, "Contact updated", {
+            ...contact,
+            updated_at: contact.updated_at,
+        });
+    } catch (error) {
+        return baseResponse(res, false, 500, "Error updating contact", error);
+    }
+};
+
+exports.deleteContact = async (req, res) => {
+    try {
+        const contact = await contactRepository.deleteContact(req.params.id);
+        if (!contact) {
+            return baseResponse(res, false, 404, "Contact not found", null);
+        }
+        return baseResponse(res, true, 200, "Contact deleted", contact);
+    } catch (error) {
+        return baseResponse(res, false, 500, "Error deleting contact", error);
+    }
+};
---
+++ src/repositories/contact.repository.js
@@ -0,0 +1,50 @@
+const db = require("../database/pg.database");
+
+exports.createContact = async (contact) => {
+    try {
+        const res = await db.query(
+            "INSERT INTO contacts (name, phone, created_at, updated_at) VALUES ($1, $2, NOW(), NOW()) RETURNING *",
+            [contact.name, contact.phone]
+        );
+        return res.rows[0];
+    } catch (error) {
+        console.error("Error executing query", error);
+        throw error;
+    }
+};
+
+exports.getContacts = async () => {
+    try {
+        const res = await db.query("SELECT * FROM contacts");
+        return res.rows;
+    } catch (error) {
+        console.error("Error executing query", error);
+        throw error;
+    }
+};
+
+exports.updateContact = async (id, contact) => {
+    try {
+        const query = `
+            UPDATE contacts 
+            SET name = $1, phone = $2, updated_at = NOW() 
+            WHERE id = $3 
+            RETURNING *`;
+        const params = [contact.name, contact.phone, id];
+        const res = await db.query(query, params);
+        return res.rows[0];
+    } catch (error) {
+        console.error("Error executing query", error);
+        throw error;
+    }
+};
+
+exports.deleteContact = async (id) => {
+    try {
+        const res = await db.query("DELETE FROM contacts WHERE id = $1 RETURNING *", [id]);
+        return res.rows[0];
+    } catch (error) {
+        console.error("Error executing query", error);
+        throw error;
+    }
+};
